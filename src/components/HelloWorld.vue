07fbabd76e33ffd382f3d733a10d1f6fab261c9d
ericstretton <eric.stretton@gmail.com>
2022-06-14 10:25:10 -0600

    Created get and post reqeusts.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,90 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input 
+    v-model="title" id="title"
+    type="text">
+    <input 
+    v-model="content" id="content"
+    type="text">
+    <input 
+    v-model="user" id="user"
+    type="text">
+    <input 
+    v-model="created_at" id="created_at"
+    type="text">
+
+    <button 
+    @click="postBlogPost"
+    >POST</button>
+    <button 
+    @click="getPosts"
+    >GET</button>
+
+    <div
+    v-for="post in postArr"
+    :key="post.animalId"
+    >
+      {{post.postId}}
+      <h1>{{post.title}}</h1>
+      <p>{{post.content}}</p>
+      <h3>{{post.user}}</h3>
+      <h3>{{post.created_at}}</h3>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      content: null,
+      title: null,
+      created_at: null,
+      user: null,
+      postArr: []
+    }
+  },
+  methods: {
+    getPosts(){
+      console.log("Running GET");
+      axios.request({
+        url : 'http://127.0.0.1:5000/api/posts',
+        method : "GET"
+      }).then((response)=>{
+        console.log(response);
+        this.postArr = response.data;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    },
+    postBlogPost() {
+      axios.request({
+        url : 'http://127.0.0.1:5000/api/posts',
+        method : "POST",
+        data : {
+          content : this.content,
+          title : this.title,
+          user : this.user,
+          created_at : this.created_at
+        },
+      }).then(()=>{
+        this.getPosts();
+      }).catch((error)=>{
+        console.log(error);
+      })
+      console.log(this.content, this.title, this.user, this.created_at);
+    },
+  },
+  mounted () {
+    this.getPosts();
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
